3ce7dc05ae58e0d86524c0e02846e544178fb035
HarshalPatel1972 <hp842484@gmail.com>
2026-02-14 16:44:02 +0530

    Reduce spacing between RealTalk and Tantra Paths sections by half

--- components/TantraTypesSection.tsx
+++ components/TantraTypesSection.tsx
@@ -146,7 +146,7 @@ export default function TantraTypesSection() {
     <>
       <section
         id="tantra-paths"
-        className="py-8 md:py-12 bg-cream scroll-mt-24 sm:scroll-mt-32"
+        className="py-4 md:py-6 bg-cream scroll-mt-24 sm:scroll-mt-32"
       >
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           {/* Section Heading */}
