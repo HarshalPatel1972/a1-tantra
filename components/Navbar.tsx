e7ee5e03f4bc827c688a318e3657671075d0bc65
HarshalPatel1972 <hp842484@gmail.com>
2026-02-14 17:59:56 +0530

    Add object-contain styling to logo images for better display

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -137,7 +137,7 @@ export default function Navbar() {
             alt="A1 Tantra Logo"
             width={scrolled ? 32 : 56}
             height={scrolled ? 32 : 56}
-            className={`flex-shrink-0 transition-all duration-700 ease-out ${
+            className={`flex-shrink-0 transition-all duration-700 ease-out object-contain ${
               scrolled
                 ? "w-[32px] md:w-[36px] lg:w-[40px] xl:w-[50px] 2xl:w-[64px] h-[32px] md:h-[36px] lg:h-[40px] xl:h-[50px] 2xl:h-[64px]"
                 : "w-[56px] md:w-[70px] lg:w-[80px] xl:w-[90px] 2xl:w-[110px] h-[56px] md:h-[70px] lg:h-[80px] xl:h-[90px] 2xl:h-[110px]"
@@ -195,7 +195,7 @@ export default function Navbar() {
             alt="A1 Tantra Logo"
             width={32}
             height={32}
-            className="w-8 h-8 flex-shrink-0"
+            className="w-8 h-8 flex-shrink-0 object-contain"
             priority
           />
           <span>A1 TANTRA</span>
