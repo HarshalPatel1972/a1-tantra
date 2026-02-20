6a8fe07a3d981a07b02eee37deb9d7d8a2fc39f1
HarshalPatel1972 <hp842484@gmail.com>
2026-02-20 20:57:21 +0530

    FIX: Import Link in TantraTypesSection to fix Vercel build error

--- components/TantraTypesSection.tsx
+++ components/TantraTypesSection.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function TantraTypesSection() {
   const [selectedTantra, setSelectedTantra] = useState<number | null>(null);
